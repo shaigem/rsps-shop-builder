209530ed70be46f271b8490a5b817c7ffb5f34f3
Shai <odante.shai@gmail.com>
2016-09-04 23:26:07 -0400

    Fix exception dialog on Linux

--- rssb-ui/src/main/java/org/bitbucket/shaigem/rssb/util/AlertDialogUtil.java
+++ rssb-ui/src/main/java/org/bitbucket/shaigem/rssb/util/AlertDialogUtil.java
@@ -1,10 +1,12 @@
 package org.bitbucket.shaigem.rssb.util;
 
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -33,9 +35,14 @@ public class AlertDialogUtil {
         expContent.setMaxWidth(Double.MAX_VALUE);
         expContent.add(label, 0, 0);
         expContent.add(textArea, 0, 1);
-
-        alert.getDialogPane().setContent(expContent);
-        alert.getDialogPane().setExpanded(true);
+        // Fixes a problem where the dialog would not resize when you expand the content
+        // ONLY OCCURS ON LINUX
+        alert.getDialogPane().expandedProperty().addListener((l) -> Platform.runLater(() -> {
+            alert.getDialogPane().requestLayout();
+            Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+            stage.sizeToScene();
+        }));
+        alert.getDialogPane().setExpandableContent(expContent);
         return alert;
     }
 
